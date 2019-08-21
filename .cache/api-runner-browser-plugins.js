a5e066c88f8b69c0a5f576bb955fd547d3576a17
rantaro <genki.meado@gmail.com>
2019-08-22 00:29:37 +0900

    ディレクトリ構造を変更

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/matsubaragenki/Documents/GitHub/eezyanaika/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":2048},
+    },{
+      plugin: require('/Users/matsubaragenki/Documents/GitHub/eezyanaika/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/Users/matsubaragenki/Documents/GitHub/eezyanaika/src/cms/cms.js"},
+    }]
