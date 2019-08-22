cb3b931799c8a509c664b9ec309b4db0319d85e1
rantaro <genki.meado@gmail.com>
2019-08-23 02:12:01 +0900

    メタ修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'ええじゃないか',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      '「ええじゃないか。」と気楽に更新しているエンジニアブログです。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,8 +11,8 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <p className="footer-title">ええじゃないかブログ</p>
+        <div className="content has-text-centered footer-title">
+          ええじゃないかブログ
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
