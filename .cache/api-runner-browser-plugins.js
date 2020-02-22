83da5edd67cafe92d6dbea31e95ea3aa5d8219a9
rantaro <genki.meado@gmail.com>
2020-02-23 02:52:50 +0900

    エラー解消

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,10 +1,10 @@
 module.exports = [{
-      plugin: require('/Users/genki.matsubara/Documents/GitHub/eezyanaika/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      plugin: require('/Users/matsubaragenki/Documents/GitHub/eezyanaika/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/genki.matsubara/Documents/GitHub/eezyanaika/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      plugin: require('/Users/matsubaragenki/Documents/GitHub/eezyanaika/node_modules/gatsby-remark-images/gatsby-browser.js'),
       options: {"plugins":[],"maxWidth":2048},
     },{
-      plugin: require('/Users/genki.matsubara/Documents/GitHub/eezyanaika/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
-      options: {"plugins":[],"modulePath":"/Users/genki.matsubara/Documents/GitHub/eezyanaika/src/cms/cms.js"},
+      plugin: require('/Users/matsubaragenki/Documents/GitHub/eezyanaika/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/Users/matsubaragenki/Documents/GitHub/eezyanaika/src/cms/cms.js"},
     }]
