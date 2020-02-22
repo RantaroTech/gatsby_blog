cd52212582770cc4b6c04e3e2ff44b37d52ab4f3
rantaro <genki.meado@gmail.com>
2020-02-23 02:57:02 +0900

    フッターのSNS修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -55,14 +55,14 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                {/* <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
-                <a title="twitter" href="https://twitter.com">
+                </a> */}
+                <a title="twitter" href="https://twitter.com/rantaro_33">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -70,20 +70,20 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                {/* <a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
                     alt="Instagram"
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
+                </a> */}
+                {/* <a title="vimeo" href="https://vimeo.com">
                   <img
                     src={vimeo}
                     alt="Vimeo"
                     style={{ width: '1em', height: '1em' }}
                   />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
